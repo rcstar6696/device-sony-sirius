aec74a07ca2bf1004d104e0dcb1f9be1a25ab98d
Max Weffers <rcstar6696@gmail.com>
2017-01-02 16:25:40 +0100

    Set fixed fps for video recording

--- device.mk
+++ device.mk
@@ -66,6 +66,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml
 
+# Cam FPS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.debug.set.fixedfps=15
+
 # Device Init
 PRODUCT_PACKAGES += \
     init.recovery.sirius \
